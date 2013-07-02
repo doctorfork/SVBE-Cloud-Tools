31d80cd1b49c00d36c17d6418cc9066fc8ac7980
Dave Fork <david.k.fork@gmail.com>
2013-07-01 20:09:27 -0700

    debugging and model refinement

--- models.py
+++ models.py
@@ -13,7 +13,6 @@ class Person(Contact):
         return self.full_name.split(' ')[-1]
     email = db.EmailProperty()
     birthday = db.DateProperty()
-    address = db.PostalAddressProperty()
     mobile_number = db.PhoneNumberProperty()
     @property 
     def age(self):
@@ -23,14 +22,13 @@ class OneOfUsPerson(Person):
     """a person having a working relationship with the organization.
     properties include a unique ID, a start date, and qualifications
     """
-    def is_youth(self)
+    def is_youth(self):
         return self.age < 18
     start_date = db.DateProperty(auto_now_add=True)
     active = db.BooleanProperty()
     independent = db.BooleanProperty()
     # roles link to the role class
     roles = db.ListProperty(item_type=db.Key)
-    events = db.ListProperty()
     volunteer_hours = db.IntegerProperty()
     volunteer_points = db.IntegerProperty()
     @property
@@ -48,41 +46,51 @@ class Event(polymodel.PolyModel):
     stop_time = db.TimeProperty()
     address = db.PostalAddressProperty()
     event_leader = db.ReferenceProperty(Person)
-    # the roles may need to be a list of [role, num_needed, [participants] [waitlist] [removed]]
-    event_roles = db.ListProperty()
+    # event_roles is a list of type EventRole
+    event_roles = db.ListProperty(item_type=db.Key)
 
 class DonationIn(db.Model):
     date = db.DateProperty()
     donor = db.ReferenceProperty(Contact)
     # list of each bike by serial #, description & est. value
     # I'd like to make a less - SVBE specific rendering of this design intent
-    bikes = db.ListProperty()
+    bikes = db.ListProperty(item_type=db.Key)
     cash = db.FloatProperty()
-    other = db.ListProperty()
+    other = db.ListProperty(item_type=db.Key)
+
+
+class Sku(db.Model):
+    pass   
 
 class Purchase(db.Model):
     date = db.DateProperty()
-    buyer = db.ReferenceProperty(Contact)
-    sku = db.ReferenceProperty(sku)
+    buyer = db.ReferenceProperty(Contact, collection_name = 'purchases')
+    sku = db.ReferenceProperty(Sku)
     amount = db.FloatProperty()
-    seller = db.ReferenceProperty(Contact)
+    seller = db.ReferenceProperty(Contact, collection_name = 'sales')
 
 class Bike(db.Model):
-    pass
-
-class sku(db.Model):
     pass
 
 class Role(db.Model):
     role_type = db.CategoryProperty()
 
+class EventRole(db.Model):
+    """role and number of people needed to fill that role for an event
+    """
+    role = db.ReferenceProperty(Role)
+    role_num = db.IntegerProperty()
+    event = db.ReferenceProperty(Event)
+
 class PersonEvent(db.Model):
     person = db.ReferenceProperty(OneOfUsPerson, required = True, collection_name = 'events')
+    #do we still need event if we track event_roll?
     event = db.ReferenceProperty(Event, required = True, collection_name = 'people')
-    title = db.StringProperty()         
+    event_roles = db.ListProperty(item_type = db.Key)
 
-p = Person(key_name = 'foof',
-           name='Dave Nielsen')
+    
+#p = Person(key_name = 'foof',
+#           name='Dave Nielsen')
 #p.name = "Dave Nielsen"
 #p.birthday = datetime.date(1988,11,12)
 #p.put()
