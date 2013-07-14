b79cda312a0e77b6d0540cb6131b64f5f906503f
David Nielsen <david.nielsen88@gmail.com>
2013-07-14 16:23:12 -0700

    Added test of PersonEventModel

--- main.py
+++ main.py
@@ -65,7 +65,25 @@ class OneOfUsPersonTest(webapp2.RequestHandler):
         pprint.pprint(db.to_dict(ooup))
         self.response.write('End of OneOfUsPersonTest')
 
+class AttendenceTest(webapp2.RequestHandler):
+    def get(self):
+        leader = models.OneOfUsPerson.get_or_insert('poof',full_name = "Alfred E. Newman")
+        leader.put()
+        event = models.Event.get_or_insert('roof',date = datetime.date(2013,7,14),
+                                            event_leader = leader)
+        event.put()
+        assist_role = models.Role.get_or_insert('Assistant',role_type = 'Assistant')
+        assist_role.put()
+        
+        person_at_event_doing_role = models.PersonEventRole(person = leader, event = event, 
+                                                            role = assist_role)
+        person_at_event_doing_role.put()
+        self.response.write(db.to_dict(person_at_event_doing_role))
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/OneOfUsPersonTest', OneOfUsPersonTest),    
+    ('/OneOfUsPersonTest', OneOfUsPersonTest),
+    ('/AttendenceTest', AttendenceTest)    
 ], debug=True)
