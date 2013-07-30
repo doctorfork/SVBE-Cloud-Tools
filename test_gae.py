00a5f42e6032f41d5f9c741aeafd8789c1b13377
Wes Alvaro <hello@wesalvaro.com>
2013-07-29 19:28:25 -0700

    Puts the ./tests location statically in the test runner.

--- test_gae.py
+++ test_gae.py
@@ -3,11 +3,10 @@ import optparse
 import sys
 import unittest
 
-USAGE = """%prog SDK_PATH TEST_PATH
-Run unit tests for App Engine apps.
+USAGE = """%prog SDK_PATH
+Runs the unit tests.
 
-SDK_PATH    Path to the SDK installation
-TEST_PATH   Path to package containing test modules"""
+SDK_PATH    Path to the SDK installation"""
 
 
 def setUpGAE(sdk_path):
@@ -19,12 +18,12 @@ def setUpGAE(sdk_path):
 if __name__ == '__main__':
     parser = optparse.OptionParser(USAGE)
     options, args = parser.parse_args()
-    if len(args) != 2:
-        print 'Error: Exactly 2 arguments required.'
+    if len(args) != 1:
+        print 'The SDK path should be the only argument.'
         parser.print_help()
         sys.exit(1)
     setUpGAE(args[0])
     loader = unittest.TestLoader()
-    suites = loader.discover(args[1])
+    suites = loader.discover('./tests')
     runner = unittest.TextTestRunner()
     runner.run(suites)
