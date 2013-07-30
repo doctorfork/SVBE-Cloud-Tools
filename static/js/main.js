681b90848c0ca166a133418667abd92aa3e174fb
Dan Manary <dmanary@google.com>
2013-07-29 18:18:15 -0700

    Eh

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,3 @@
-function ctrl($scope) { //, Event) {
-  //$scope.foo = Event.promise;
-  $scope.foo = 1;
-}
-
-function Event($http, $log) {
-  this.promise = $http.get("http://jsbin.com/ojabuv/1/js").then(function(response) {
-    $log.info(response.data);
-    return response.data;
-  });
-  
-}
-
 function Person($http, $log) {
   this.create = function(json) {
     return $http.post("/person", json);
