f001577113277224db885632abf58a9b1ff60f23
jake <jacob@Homes-Mac-mini.local>
2013-08-25 20:24:33 -0700

    Let the Pick Event page use the new bootstrap.

--- static/js/pick_event_controller.js
+++ static/js/pick_event_controller.js
@@ -1,14 +1,10 @@
 function PickEventController($scope, $log, $http) {
   $scope.events = [];
-  $scope.eventKey = "";
+  $scope.eventKey = '';
 
   $http.get('/api/event/list').success(function(data) {
     $scope.events = data;
   });
-  
-  $scope.setActiveEvent = function($event, key) {
-    $scope.activeEvent = key;
-  };
   
   $scope.getHumanReadableDate = function(dateStr) {
     return new Date(dateStr).toLocaleDateString();
