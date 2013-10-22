0170eeaad0fc4844fcd735d6916da54ab81d63ca
David Nielsen <david.nielsen88@gmail.com>
2013-10-21 18:44:05 -0700

    Chains the routeProvider configurations.

--- static/js/svbe.js
+++ static/js/svbe.js
@@ -3,36 +3,37 @@ angular.module("SVBE", ['ui.bootstrap'])
   .service("DefaultConfigsService", DefaultConfigs)
   .service('EventService', EventService)
   .config(function($routeProvider, $locationProvider) {
-    $routeProvider.when('/event/:key/edit', {
-       templateUrl: 'event.html',
-       controller: EventController,
-       resolve: {
+    $routeProvider
+      .when('/event/:key/edit', {
+        templateUrl: 'event.html',
+        controller: EventController,
+        resolve: {
           event: function($route, EventService) {
             var key = $route.current.params['key'];
             return EventService.get(key);
           }
         }
-      });
-    $routeProvider.when('/createPerson', {
-      templateUrl: 'create_one_of_us_person.html',
-      controller: PersonController
-    });
-    $routeProvider.when('/pickEvent', {
-      templateUrl: 'pick_event.html',
-      controller: PickEventController
-    });
-    $routeProvider.when('/event/:key', {
-      templateUrl: 'add_participants_to_event.html',
-      controller: AddParticipantsToEventController,
-      resolve: {
-        event: function($route, EventService) {
-          var key = $route.current.params['key'];
-          return EventService.get(key);
+      })
+      .when('/createPerson', {
+        templateUrl: 'create_one_of_us_person.html',
+        controller: PersonController
+      })
+      .when('/pickEvent', {
+        templateUrl: 'pick_event.html',
+        controller: PickEventController
+      })
+      .when('/event/:key', {
+        templateUrl: 'add_participants_to_event.html',
+        controller: AddParticipantsToEventController,
+        resolve: {
+          event: function($route, EventService) {
+            var key = $route.current.params['key'];
+            return EventService.get(key);
+          }
         }
-      }
-    });
-    $routeProvider.otherwise({
-      templateUrl: 'things_to_do.html'
-    });
+      })
+      .otherwise({
+        templateUrl: 'things_to_do.html'
+      });
     //$locationProvider.html5Mode(true);
   });
