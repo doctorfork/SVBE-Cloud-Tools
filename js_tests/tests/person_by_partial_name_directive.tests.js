dff2aea3176a6e1d012fb5598301777517fd90a6
Jake Ratkiewicz <jacob.ratkiewicz@gmail.com>
2013-11-04 19:23:22 -0800

    Finish adding tests for the PersonByPartialNameDirective.

--- js_tests/tests/person_by_partial_name_directive.tests.js
+++ js_tests/tests/person_by_partial_name_directive.tests.js
@@ -4,21 +4,72 @@ function dumpElement(elt) {
 }
 
 describe('person by partial name directive works', function() {
-  var element, scope;
+  var element, scope, httpBackend;
+
+  var personObject = [
+    {
+      "fullName": "Stanley Q. Fakerton",
+      "birthday": "2013-11-05T00:00:00.000000Z",
+      "key": "ahNkZXZ-YWN0aXZlLWJpcmQtMjU2chQLEgdDb250YWN0GICAgICAgIAKDA",
+      "mobileNumber": "555-1212",
+      "email": "stan@fake.com",
+      "address": "123 Anywhere St.",
+      "roles": [
+        {
+          "roleInfoURL": null,
+          "roleType": "Apprentice",
+          "roleBriefDescription": null,
+          "key": "ahNkZXZ-YWN0aXZlLWJpcmQtMjU2chQLEgRSb2xlIgpBcHByZW50aWNlDA"
+        }
+      ],
+      "class": [
+        "Contact",
+        "Person",
+        "OneOfUsPerson"
+      ],
+      "phoneNumber": "555-2323",
+      "volunteerPoints": null,
+      "emergencyContact": null,
+      "responsibleAdult": null,
+      "independent": null,
+      "volunteerHours": null
+    }
+  ];
   
   beforeEach(module('SVBE'));
   beforeEach(module('templates'));
   
-  beforeEach(inject(function($rootScope, $compile, $templateCache) {
+  beforeEach(inject(function($rootScope, $compile, $httpBackend) {
     element = angular.element(
-      '<person-by-partial-name selected-person="person"/>');
+      '<person-by-partial-name selected-person="exteriorPerson"/>');
     scope = $rootScope.$new();
     element = $compile(element)(scope);
-    scope.$digest();
-    console.log(dumpElement(element));
+    $rootScope.$digest();
+    
+    httpBackend = $httpBackend;
   }));
+  
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
   
   it('compiles', function() {
     expect(element.find('form').length).toBe(1);
+  });
+  
+  it('populates selected person', function() {
+    httpBackend.expectGET('/api/person/by_name/foo').
+        respond(201, personObject);
+    
+    element.find('input#person-name').val('foo').trigger('input');
+    element.find('#search-btn').click();
+    httpBackend.flush();
+    
+    var btnSelector = element.find('#candidate-people-buttons > button');
+    expect(btnSelector.length).toEqual(1);
+    expect(scope.exteriorPerson).not.toBeDefined();
+    btnSelector.click();
+    expect(scope.exteriorPerson).toBeDefined();
   });
 });
--- static/js/person_by_partial_name_directive.js
+++ static/js/person_by_partial_name_directive.js
@@ -7,17 +7,13 @@ function PersonByPartialNameDirective($http, $log) {
     templateUrl: 
         '/static/html/person_by_partial_name_directive_template.html',
     link: function(scope, element, attr) {
-        console.log(scope.selectedPerson)
-    
       // Search function to find a person by their name or a fragment thereof.
       scope.getPeopleByPartialName = function() {
-        $log.info('prefix: ' + scope.personSearchName);
         $http.get('/api/person/by_name/' + scope.personSearchName).success(function(data) {
           scope.people = data;
         });
       };
       scope.select = function(person) {
-        console.log(scope.selectedPerson, person)
         scope.selectedPerson = person;
       };
     }
