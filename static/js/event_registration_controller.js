716590f8bde68ad273250d5456496465d786eda6
Wes Alvaro <hello@wesalvaro.com>
2013-12-10 19:28:24 -0800

    Format cleanup.

--- static/js/event_registration_controller.js
+++ static/js/event_registration_controller.js
@@ -1,19 +1,20 @@
-function EventRegistrationController($scope, $http, $timeout) {
-  $scope.personSearchName = "";
+function EventRegistrationController($scope, $http, $log, $timeout) {
+  $scope.personSearchName = '';
   $scope.selectedPerson = null;
   $scope.possiblePeople = {};
-  $scope.registrationError = "";  // Error message from registration attempt.
+  $scope.registrationError = '';  // Error message from registration attempt.
   
   $scope.eventRoles = {}; // Maps role-types to counts (what event needs)
   $scope.personEventRoles = {}; // Maps role-types to counts (what event has)
 
   // Fetch the person event role counts (what this event has).
   $scope.fetchPersonEventRoles = function() {
-    $http.get('/api/person_event_roles/get_summary_by_event/' + 
-              $scope.event.key).success(function(data) {
-                $scope.personEventRoles = data;
-              });  
+    $http.get('/api/person_event_roles/get_summary_by_event/' + $scope.event.key).
+      success(function(data) {
+        $scope.personEventRoles = data;
+      });  
   };
+  // TODO(wesalvaro): Fetch this in a resolve.
   $scope.fetchPersonEventRoles();
   
   $scope.getPossibleRolesForSelectedPerson = function() {
@@ -21,7 +22,7 @@ function EventRegistrationController($scope, $http, $timeout) {
     
     var personRoles = $scope.selectedPerson.roles;
     var neededRoles = [];
-    console.log(' person roles', personRoles);
+    $log.info('Person roles:', personRoles);
     for (var i = 0; i < personRoles.length; ++i) {
       if ($scope.event.roles[personRoles[i]['roleType']] > 0) {
         // Then we need at least one more of these.
@@ -33,20 +34,19 @@ function EventRegistrationController($scope, $http, $timeout) {
   
   // Register the currently selected person for this event.
   $scope.registerPerson = function(role) {
-    $http.post('/api/event/register_person',
-      {'eventKey': $scope.event.key, 
-       'personKey': $scope.selectedPerson.key,
-       'roleKey': role}).success(
-        function() {
-          $scope.selectedPerson = null;
-          $scope.personSearchName = ""; 
-          $scope.possiblePeople = new Object();
-          // Re-fetch this event's registration so we see the new registrant.
-          console.log('About to fetch more person event roles.');
-          $timeout(function() { $scope.fetchPersonEventRoles(); }, 500);
-        }).error(
-        function(data) {
-          $scope.registrationError = data;
-        });
+    $http.post('/api/event/register_person', {
+        'eventKey': $scope.event.key, 
+        'personKey': $scope.selectedPerson.key,
+        'roleKey': role
+      }).then(function() {
+        $scope.selectedPerson = null;
+        $scope.personSearchName = ''; 
+        $scope.possiblePeople = new Object();
+        // Re-fetch this event's registration so we see the new registrant.
+        $log.info('About to fetch more person event roles.');
+        $timeout(function() { $scope.fetchPersonEventRoles(); }, 500);
+      }, function(data) {
+        $scope.registrationError = data;
+      });
   };
 }
