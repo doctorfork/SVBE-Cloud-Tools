c816b0a1da68c2e5785b6116b6892812299c598e
jake ratkiewicz <jacob.ratkiewicz@gmail.com>
2014-01-06 18:41:54 -0800

    Make app_definition.py pass pylint.

--- app_definition.py
+++ app_definition.py
@@ -1,3 +1,9 @@
+"""Definition for the entire SVBE app.
+
+Lists all the handler paths and their classes."""
+
+# F0401:  9,0: Unable to import 'webapp2'
+# pylint: disable=F0401
 import webapp2
 
 import event_handler
@@ -8,6 +14,8 @@ import main_handler
 import roles_handlers
 
 def GetSVBEApp():
+    """Returns the SVBE WSGI application."""
+
     return webapp2.WSGIApplication([
         ('/', main_handler.MainHandler),
         ('/LoadRoles', roles_handlers.LoadRoles),
@@ -17,10 +25,11 @@ def GetSVBEApp():
         ('/api/event/(.+)', event_handler.EventHandler),
         ('/api/person/list', person_handlers.GetPersonListHandler),
         ('/api/person', person_handlers.SavePersonHandler),
-        ('/api/person/by_name/(.+)', person_handlers.GetPersonByNameAndEmailHandler),
+        ('/api/person/by_name/(.+)',
+         person_handlers.GetPersonByNameAndEmailHandler),
         ('/api/person/(.+)', person_handlers.GetPersonByIdHandler),
         ('/api/roles/get', roles_handlers.GetRoleTypes),
-        ('/api/event_roles/get_by_event/(.+)', 
+        ('/api/event_roles/get_by_event/(.+)',
          event_role_handler.GetEventRolesByEventHandler),
         ('/api/person_event_roles/get_summary_by_event/(.+)',
          person_event_role_handler.GetPersonEventRolesSummaryByEventHandler),
