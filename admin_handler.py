901266e1f6cefa1759f796fb3c0d0e6c0ff06c46
David Nielsen <david.nielsen88@gmail.com>
2014-01-13 19:59:15 -0800

    Use commas instead of pipes for role separators to match Google Forms.

--- admin_handler.py
+++ admin_handler.py
@@ -12,14 +12,14 @@ def UploadPeople(request):
   
   Example:
   phoneNumber,address,fullName,email,birthday,mobileNumber,roles
-  1234,123 Main,Stan Fakerton,stan@fake.com,2014-01-14T03:31:15.138Z,5678,Apprentice|Sales
+  1234,123 Main,Stan Fakerton,stan@fake.com,2014-01-14T03:31:15.138Z,5678,"Apprentice,Sales"
   
   Args:
     request: A webapp2 request object.
   """
   for row in csv.DictReader(request.POST['file'].file):
     # Reformat roles from CSV string:
-    row['roles'] = [{'roleType': role.strip()} for role in row['roles'].split('|')]
+    row['roles'] = [{'roleType': role.strip()} for role in row['roles'].split(',')]
     try:
       person_handlers.SavePerson(row)
     except ValueError as e:
