9b7115b4b441cbbb2b4d5b85ee0c2405c2c125b0
jake ratkiewicz <jacob.ratkiewicz@gmail.com>
2014-01-27 18:13:57 -0800

    Cause event_role_handler to not have lint warnings.

--- event_role_handler.py
+++ event_role_handler.py
@@ -1,18 +1,27 @@
-import webapp2
+"""Server methods pertaining to EventRoles."""
+
+# Class has no __init__ method (no-init)
+# pylint: disable=W0232
+# Class 'Event' has no 'get' member (no-member)
+# pylint: disable=E1101
+# Too few public methods (1/2) (too-few-public-methods)
+# pylint: disable=R0903
+
+import webapp2 # pylint: disable=F0401
 import models
 import json
 
 class GetEventRolesByEventHandler(webapp2.RequestHandler):
     def get(self, event_key):
         """Fetches a list of event roles for an event, given the key.
-        
+
         Returns a list of roles and counts, JSON serialized."""
         print ':%s:' % event_key
         event = models.Event.get(event_key)
-        if not event: 
+        if not event:
             self.error(404)
             return
-    
+
         self.response.write(json.dumps(
-            [(event_role.role.ToDict(), event_role.role_num) 
+            [(event_role.role.ToDict(), event_role.role_num)
              for event_role in event.eventrole_set.run()]))
