2efaa137cd12b7cf04d9f0d4a2ca9c1a069854a2
jake ratkiewicz <jacob.ratkiewicz@gmail.com>
2014-01-27 19:29:53 -0800

    Fix (or explicitly ignore) lint errors in admin_handlers

--- admin_handler.py
+++ admin_handler.py
@@ -1,29 +1,33 @@
-import models
+"""HTTP handlers for admin methods"""
+
 import csv
 import person_handlers
 
 def UploadPeople(request):
-  """Bulk saves a CSV of Person records to the Datastore.
+    """Bulk saves a CSV of Person records to the Datastore.
 
-  A form POST to this handler should contain a file in the field "file".
-  There should be a header row with the field names matching the JSON properties.
-  The "roles" field, however, should simply be a pipe ("|") separated list of
-  role types to be saved with the Person.
+    A form POST to this handler should contain a file in the field
+    "file".  There should be a header row with the field names
+    matching the JSON properties.  The "roles" field, however, should
+    simply be a pipe ("|") separated list of role types to be saved
+    with the Person.
 
-  Example:
-  phoneNumber,address,fullName,email,birthday,mobileNumber,roles
-  1234,123 Main,Stan Fakerton,stan@fake.com,2014-01-14T03:31:15.138Z,5678,"Apprentice,Sales"
+    Example:
+    phoneNumber,address,fullName,email,birthday,mobileNumber,roles
+    1234,123 Main,Stan Fakerton,stan@fake.com,2014-01-14T03:31:15.138Z,5678,"Apprentice,Sales"
 
-  Args:
-    request: A webapp2 request object.
-  """
-  for row in csv.DictReader(request.POST['file'].file):
-    # Reformat roles from CSV string:
-    row['roles'] = [{'roleType': role.strip()} for role in row['roles'].split(',')]
-    try:
-      person_handlers.SavePerson(row)
-    except ValueError as e:
-      print e.args[0]
+    Args:
+      request: A webapp2 request object.
+
+    """
+    for row in csv.DictReader(request.POST['file'].file):
+        # Reformat roles from CSV string:
+        row['roles'] = [{'roleType': role.strip()}
+                        for role in row['roles'].split(',')]
+        try:
+            person_handlers.SavePerson(row)
+        except ValueError as ex:
+            print ex.args[0]
 
 HANDLERS = [
     ('/admin/person/upload', UploadPeople),
