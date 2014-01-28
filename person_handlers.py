5e63e578e93c599f2288e253c5a8d3fd7b119852
jake ratkiewicz <jacob.ratkiewicz@gmail.com>
2014-01-27 19:12:51 -0800

    Fix or explicitly ignore the rest of the lint errors in person_handlers

--- person_handlers.py
+++ person_handlers.py
@@ -12,7 +12,6 @@
 import webapp2 # pylint: disable=F0401
 import models
 import json
-import datetime
 import utils
 import re
 from webob import exc
@@ -88,8 +87,8 @@ def _UpdateRoles(person, new_roles):
     """
     print new_roles
     existing_roles = {
-        r.role.role_type : r
-        for r in models.PersonRole.all().filter('person = ', person)}
+        person_role.role.role_type : person_role
+        for person_role in models.PersonRole.all().filter('person = ', person)}
     new_roles = {role['roleType'] : role for role in new_roles}
 
     all_roles = set(existing_roles.keys()) | set(new_roles.keys())
@@ -130,69 +129,72 @@ def _UpdateRoles(person, new_roles):
 def _GetPersonByEmail(email):
     return models.OneOfUsPerson.all().filter("email = ", email).get()
 
-def SavePerson(person):
-    def check_required_fields(person):
-        # Check required fields.
-        for field_name in ['fullName', 'birthday', 'roles', 'email']:
-            if not field_name in person:
-                raise ValueError(u'Missing required field ' + field_name)
+def SavePerson(person_json):
+    """Saves the given person, or raises a ValueError."""
 
-    def check_valid_email(email):
-        if not IsValidEmail(email):
-            raise ValueError(u'Not a valid email address: %s' % email)
+    # Check required fields.
+    for field_name in ['fullName', 'birthday', 'roles', 'email']:
+        if not field_name in person_json:
+            raise ValueError(u'Missing required field ' + field_name)
 
-    def check_duplicate_email(p, email):
-        # See if there's already a person with the same email.
-        dup = _GetPersonByEmail(email)
-        if dup and (not p.is_saved() or dup.key() != p.key()):
-            raise ValueError(
-                u"There's already a person with that email (%s)" % email)
+    # Check if the given email is valid.
+    if not IsValidEmail(person_json['email']):
+        raise ValueError(u'Not a valid email address: %s' %
+                         person_json['email'])
 
-    check_required_fields(person)
-    check_valid_email(person['email'])
-
-    if 'key' in person:
-        p = models.OneOfUsPerson.get(person['key'])
-        p.full_name = person['fullName']
-        p.birthday = utils.ParseISODate(person['birthday']).date()
-        p.email = person['email']
+    if 'key' in person_json:
+        person = models.OneOfUsPerson.get(person_json['key'])
+        person.full_name = person_json['fullName']
+        person.birthday = utils.ParseISODate(person_json['birthday']).date()
+        person.email = person_json['email']
     else:
-        p = models.OneOfUsPerson(
-                full_name=person['fullName'],
-                birthday=utils.ParseISODate(person['birthday']).date(),
-                email=person['email'])
+        person = models.OneOfUsPerson(
+                full_name=person_json['fullName'],
+                birthday=utils.ParseISODate(person_json['birthday']).date(),
+                email=person_json['email'])
 
-    check_duplicate_email(p, person['email'])
+    # See if there's already a person with the same email.
+    dup = _GetPersonByEmail(person_json['email'])
+    if dup and (not person.is_saved() or dup.key() != person.key()):
+        raise ValueError(
+            u"There's already a person with that email (%s)" %
+            person_json['email'])
 
     # Populate optional fields, if the data is present.
-    if person.get('phoneNumber'):
+    if person_json.get('phoneNumber'):
         # TODO(attackcowboy): Maybe validate?
-        p.phone_number = person['phoneNumber']
+        person.phone_number = person_json['phoneNumber']
 
-    if person.get('address'):
-        p.address = person['address']
+    if person_json.get('address'):
+        person.address = person_json['address']
     else:
-        p.address = None
+        person.address = None
 
-    if person.get('mobileNumber'):
-        p.mobile_number = person['mobileNumber']
+    if person_json.get('mobileNumber'):
+        person.mobile_number = person_json['mobileNumber']
 
-    p.put()
-    _UpdateRoles(p, person['roles'])
-    return p
+    person.put()
+    _UpdateRoles(person, person_json['roles'])
+    return person
 
 
 class SavePersonHandler(webapp2.RequestHandler):
     def post(self):
+        """Attempts to edit an existing person, or create a new one.
+
+        If the 'key' field is present in the given JSON, attempts to edit
+        the person with that key; otherwise, creates a new person. If a key
+        is given but no person exists with that key, throws a ValueError."""
+
         person = json.loads(self.request.body)
         try:
-            p = SavePerson(person)
-        except ValueError as e:
+            person = SavePerson(person)
+        except ValueError as ex:
             response = exc.HTTPBadRequest()
             response.content_type = 'text/plain'
-            response.text = e.args[0]
+            response.text = ex.args[0]
             raise response
-        self.response.write(utils.CreateJsonFromModel(p))
+        self.response.write(utils.CreateJsonFromModel(person))
 
 HANDLERS = [
     ('/api/person/list', GetPersonListHandler),
