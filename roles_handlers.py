eb3a9e42a5c49038eb4a0965b70aee25fe6b7e10
jake ratkiewicz <jacob.ratkiewicz@gmail.com>
2014-01-27 19:16:40 -0800

    Fix or explicitly ignore all pylint errors in roles_handlers

--- roles_handlers.py
+++ roles_handlers.py
@@ -1,49 +1,61 @@
-import webapp2
+"""HTTP handlers for Role objects."""
+
+# Class has no __init__ method (no-init)
+# pylint: disable=W0232
+#
+# Instance of 'EventListHandler' has no 'response' member (no-member)
+# pylint: disable=E1101
+#
+# Too few public methods (1/2) (too-few-public-methods)
+# pylint: disable=R0903
+
+import webapp2 # pylint: disable=F0401
 import models
 import json
 
 class GetRoleTypes(webapp2.RequestHandler):
-  def get(self):
-    """Returns all the valid roles."""
-    self.response.content_type = 'application/json'
-    self.response.write(json.dumps([r.role_type for r in models.Role.all()]))
+    def get(self):
+        """Returns all the valid roles."""
+        self.response.content_type = 'application/json'
+        self.response.write(json.dumps(
+            [role.role_type for role in models.Role.all()]))
 
-               
+
 class LoadRoles(webapp2.RequestHandler):
-  def get(self):
-    # Role Names to put into datastore:
-    role_names = [
-      "Accountant",
-      "Announcements",
-      "Apprentice",
-      "Cleaning",
-      "Course Instructor",
-      "Delivery",
-      "Director",
-      "Event Leader",
-      "Event Setup",
-      "Event Wrapup",
-      "Food Coordinator",
-      "Homework Mechanic",
-      "Intake",
-      "Inventory",
-      "Mechanic",
-      "Mentor",
-      "Officer",
-      "Orientation",
-      "Outgoing Donations",
-      "Photographer",
-      "Prequal",
-      "President",
-      "Record Keeper",
-      "Recycling",
-      "Registration",
-      "Sales",
-      "Secretary",
-      "Treasurer",
-      "Volunteer Coordinator",]
-    for r in role_names:
-      role = models.Role(key_name=r,role_type=r)
-      role.put()
-      self.response.write('Added role: %s <br/>' % r)
-    self.response.write('Roles Loaded.<br/>')
+    def get(self):
+        """Initialized the database with the names of all valid roles."""
+        role_names = [
+            "Accountant",
+            "Announcements",
+            "Apprentice",
+            "Cleaning",
+            "Course Instructor",
+            "Delivery",
+            "Director",
+            "Event Leader",
+            "Event Setup",
+            "Event Wrapup",
+            "Food Coordinator",
+            "Homework Mechanic",
+            "Intake",
+            "Inventory",
+            "Mechanic",
+            "Mentor",
+            "Officer",
+            "Orientation",
+            "Outgoing Donations",
+            "Photographer",
+            "Prequal",
+            "President",
+            "Record Keeper",
+            "Recycling",
+            "Registration",
+            "Sales",
+            "Secretary",
+            "Treasurer",
+            "Volunteer Coordinator"]
+        for name in role_names:
+            role = models.Role(key_name=name, role_type=name)
+            role.put()
+            self.response.write('Added role: %s <br/>' % name)
+        self.response.write('Roles Loaded.<br/>')
